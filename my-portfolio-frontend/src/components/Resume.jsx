ca8894c88b4b4c93e10f82c6c5b6d3ac076951ca
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2026-04-15 14:07:12 +0530

    resume updated

--- my-portfolio-frontend/src/components/Resume.jsx
+++ my-portfolio-frontend/src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from '../assets/resume/DivyanshuResume_Developer.pdf'
+import resume from '../assets/resume/divyanshu_resume__1_.pdf'
 const Resume = () => {
   return (
     <div>
